8e73fd11824c469ac57b9bf5dd7f9a7720f85661
Johnny <butter3126@gmail.com>
2022-04-25 02:21:43 +0800

    feat: cart item component

---
+++ src/components/CartItemRow.tsx
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from 'react';
+
+import {
+  TableRow,
+  TableCell,
+  Select,
+  SelectChangeEvent,
+  MenuItem,
+} from '@mui/material';
+
+import { ApiError, CartsService, CartItem } from '../services/elibraryAPI';
+
+const { cartsApiPutCart, cartsApiDeleteCart } = CartsService;
+
+interface ICartItemRowProp {
+  item: CartItem
+}
+
+export const CartItemRow = (props: ICartItemRowProp) => {
+  const [hidden, setHidden] = useState('table-row');
+  const [amount, setAmount] = useState(0);
+
+  const handleChange = (event: SelectChangeEvent<number>) => {
+    let selectedNum = event.target.value as number
+
+    if (selectedNum === 0) {
+      cartsApiDeleteCart({cart_id: props.item.id})
+        .then(() => {
+          setHidden('none');
+        })
+        .catch((err: ApiError) => console.error(err))
+        .finally(() => { return; });
+    }
+
+    cartsApiPutCart({book_id: props.item.book.id, amount: selectedNum})
+      .then(() => { setAmount(selectedNum); })
+      .catch((err: ApiError) => console.error(err))
+  };
+
+  useEffect(() => {
+    setAmount(props.item.amount);
+  // eslint-disable-next-line
+  }, []);
+
+  return (
+    <React.Fragment>
+      <TableRow sx={{ '& > *': { borderBottom: 'unset' }, display: hidden }}>
+        <TableCell></TableCell>
+        <TableCell align="right">{props.item.book.name}</TableCell>
+        <TableCell align="right">{props.item.price}</TableCell>
+        <TableCell align="right">
+          <Select
+            labelId="book-amount-label"
+            id="book-amount"
+            value={amount}
+            label="amount"
+            onChange={handleChange}
+          >
+            { [0, 1, 2, 3, 4, 5].map((num: number) => <MenuItem value={num}>{num}</MenuItem>) }
+          </Select>
+        </TableCell>
+      </TableRow>
+    </React.Fragment>
+  );
+}
