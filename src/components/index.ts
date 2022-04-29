876399ec57eb6a9b6b1f83c0b015745c9e6a27f9
Johnny <butter3126@gmail.com>
2022-04-30 00:04:42 +0800

    feat: add braintree drop in

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export { Book } from './Book';
+export { BraintreeDropIn } from './BraintreeDropIn';
 export { CartItemRow } from './CartItemRow'
 export { LoginDialog } from './LoginDialog';
 export { Navbar } from './Navbar';
